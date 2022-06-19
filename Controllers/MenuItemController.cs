0d2ed263cd486fc038d3c9b1d8133505316d522c
Dinesh <dineshvasanth2000@gmail.com>
2022-06-19 16:49:50 +0530

    first commit

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MenuItemWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuItemWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MenuItemController : ControllerBase
+
+    {
+        public readonly List<MenuItem> menuItems = new List<MenuItem>()
+                        {
+                            new MenuItem{
+                                Id = 1,
+                                Name = "Dosa",
+                                FreeDelivery = true,
+                                Price = 70,
+                                DateOfLaunch = System.DateTime.Parse("2000/01/02"),
+                                Active = true,
+                            },
+                            new MenuItem{
+                                Id = 2,
+                                Name = "Briyani",
+                                FreeDelivery = true,
+                                Price = 120,
+                                DateOfLaunch = System.DateTime.Parse("2010/01/02"),
+                                Active = true,
+                            },
+                            new MenuItem{
+                                Id = 3,
+                                Name = "Falooda",
+                                FreeDelivery = false,
+                                Price = 100,
+                                DateOfLaunch = System.DateTime.Parse("2015/02/10"),
+                                Active = true,
+                            }
+                        };
+
+
+
+
+        [HttpGet]
+        public IActionResult GetMenuItem()
+        {
+            return Ok(menuItems);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMenuItemById(int id)
+        {
+            foreach (var item in menuItems)
+            {
+                if (item.Id == id)
+                    return Ok(item);
+            }
+            return BadRequest();
+        }
+
+
+
+    }
+}
